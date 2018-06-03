bddb8c7095b0f8a3e2cb5ee0c6b0e0b0e0257678
Axel Rindle <axel.rindle@gmx.de>
2018-06-03 19:02:06 +0200

    Implement randomizing the order of messages

--- src/main/kotlin/de/axelrindle/broadcaster/BroadcastingThread.kt
+++ src/main/kotlin/de/axelrindle/broadcaster/BroadcastingThread.kt
@@ -1,5 +1,6 @@
 package de.axelrindle.broadcaster
 
+import org.apache.commons.lang.math.RandomUtils
 import org.bukkit.Bukkit
 
 /**
@@ -11,6 +12,8 @@ object BroadcastingThread {
     private var id: Int = 0
     private var index = 0
     private var maxIndex: Int = 0
+    private var randomize: Boolean = false
+    private var lastRandomIndex: Int = 0
     internal var running = false
         private set
 
@@ -33,8 +36,9 @@ object BroadcastingThread {
 
     private fun getRunnable(plugin: Broadcaster, messages: List<String>): Runnable {
         maxIndex = messages.size
+        randomize = plugin.configuration.getBoolean("Cast.Randomize")
         return Runnable {
-            var message = messages[index]
+            var message = if (randomize) getRandomMessage(messages) else messages[index]
             message = Formatter.format(plugin, message)
 
             val prefix = Formatter.formatColors(plugin.configuration.getString("Cast.Prefix"))
@@ -45,8 +49,32 @@ object BroadcastingThread {
                 Bukkit.getServer().broadcastMessage(prefix + message)
             }
 
+            // don't change index if randomizing
+            if (randomize) return@Runnable
             index++
             if (index == maxIndex) index = 0
+        }
+    }
+
+    /**
+     * Returns a random message from a list.
+     *
+     * @param messages A [List] of messages.
+     * @return A randomly selected message.
+     */
+    private fun getRandomMessage(messages: List<String>): String {
+        // get a random index
+        val rand = RandomUtils.nextInt(messages.size)
+
+        // make sure no message appears twice in a row
+        return if (rand == lastRandomIndex)
+            // if we got the same index as before, generate a new one
+            // Note: using recursion here
+            getRandomMessage(messages)
+        else {
+            // we got a different index, so save it and return the appropriate message
+            lastRandomIndex = rand
+            messages[rand]
         }
     }
 
