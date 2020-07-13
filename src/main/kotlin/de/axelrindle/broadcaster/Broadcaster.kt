8d1a28948104d8ee08699fb7eb4d6bacb0bf527e
Axel Rindle <mail@axelrindle.de>
2020-07-13 22:21:06 +0200

    Create example file for json message type

--- src/main/kotlin/de/axelrindle/broadcaster/Broadcaster.kt
+++ src/main/kotlin/de/axelrindle/broadcaster/Broadcaster.kt
@@ -4,9 +4,13 @@ import de.axelrindle.broadcaster.command.BrcCommand
 import de.axelrindle.pocketknife.PocketCommand
 import de.axelrindle.pocketknife.PocketConfig
 import de.axelrindle.pocketknife.PocketLang
+import org.apache.commons.io.IOUtils
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.nio.charset.StandardCharsets
 
 /**
  * The main plugin class. Does initialization and config loading.
@@ -48,6 +52,9 @@ class Broadcaster : JavaPlugin() {
             return
         }
 
+        // manually create example json file
+        createJsonExample()
+
         // localization init
         logger.info("Loading localization...")
         localization.addLanguages("en", "de")
@@ -86,6 +93,37 @@ class Broadcaster : JavaPlugin() {
         }
 
         logger.info("Done! v${description.version}")
+    }
+
+    private fun createJsonExample() {
+        val indicatorFile = File(dataFolder, "examples_created")
+        if (indicatorFile.exists().not()) {
+            var success = true
+
+            val jsonDir = File(dataFolder, "json")
+            success = success && jsonDir.mkdir()
+
+            val jsonFile = File(dataFolder, "json/visit-github.json")
+            success = success && jsonFile.createNewFile()
+            FileOutputStream(jsonFile).use { fos ->
+                javaClass.getResourceAsStream("/json/visit-github.json").use { fis ->
+                    fis.copyTo(fos)
+                }
+            }
+
+            success = success && indicatorFile.createNewFile()
+            FileOutputStream(indicatorFile).use { fos ->
+                IOUtils.write("This file indicates that example files have been created " +
+                        "and do not need to be created again. For now, this only includes " +
+                        "the json/ directory.", fos, StandardCharsets.UTF_8)
+            }
+
+            if (success) {
+                logger.info("Created example file at ${jsonFile.absolutePath}.")
+            } else {
+                logger.warning("Failed to create example file!")
+            }
+        }
     }
 
     override fun onDisable() {
